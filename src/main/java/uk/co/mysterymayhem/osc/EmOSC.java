f5abb9049b76b8893082e10ee544c8bc36a210ab
Thomas Barlow <github@mysterymayhem.co.uk>
2021-08-01 17:11:50 +0100

    Convert most main static functions to instance methods

--- src/main/java/uk/co/mysterymayhem/osc/EmOSC.java
+++ src/main/java/uk/co/mysterymayhem/osc/EmOSC.java
@@ -16,19 +16,50 @@ import java.util.zip.GZIPOutputStream;
  * Created by Mysteryem on 31/07/2021.
  */
 public class EmOSC {
+    private String fileName;
+    private int portIn;
+    private int portOut;
+    private int recordingDurationSeconds;
+    // 'marionette' is VMC terminology
+    // Currently unused as "localhost" is being assumed
+    private String marionetteAddress;
+
+    public EmOSC(int portIn, int recordingDurationSeconds, String fileName) {
+        this.portIn = portIn;
+        this.recordingDurationSeconds = recordingDurationSeconds;
+        this.fileName = fileName;
+    }
+
+    public EmOSC(String fileName, int portOut, String marionetteAddress) {
+        this.fileName = fileName;
+        this.portOut = portOut;
+        this.marionetteAddress = marionetteAddress;
+    }
+
+    public EmOSC(int portIn, int portOut, int recordingDurationSeconds, String marionetteAddress) {
+        this.portIn = portIn;
+        this.portOut = portOut;
+        this.recordingDurationSeconds = recordingDurationSeconds;
+        this.marionetteAddress = marionetteAddress;
+    }
 
     public static void main(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Missing arguments");
+        }
         try {
             switch (args[0].toLowerCase()) {
                 case "record":
-                    recordToFile(args);
+                    EmOSC.recordToFile(args);
                     break;
                 case "play":
-                    playFromFile(args);
+                    EmOSC.playFromFile(args);
                     break;
                 case "inout":
-                    recordAndPlayback(args);
+                    EmOSC.recordAndPlayback(args);
                     break;
+                default:
+                    throw new IllegalArgumentException("Unrecognised argument '" + args[0] + '"');
             }
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
@@ -43,8 +74,12 @@ public class EmOSC {
         int portIn = Integer.parseInt(args[2]);
         int recordingTimeSeconds = Integer.parseInt(args[3]);
 
-        recordingCountdown();
-        saveToFile(record(portIn, recordingTimeSeconds), fileName);
+        EmOSC emOSC = new EmOSC(portIn, recordingTimeSeconds, fileName);
+
+        EmOSC.recordingCountdown();
+        List<RecordedMessage> recordedMessages = emOSC.record();
+
+        emOSC.saveToFile(recordedMessages);
     }
 
     private static void playFromFile(String[] args) throws IOException {
@@ -52,7 +87,13 @@ public class EmOSC {
         int portOut = Integer.parseInt(args[2]);
         String marionetteAddress = args.length > 3 ? args[3] : "localhost";
 
-        playback(loadFromFile(fileName), portOut, marionetteAddress);
+        EmOSC emOSC = new EmOSC(fileName, portOut, marionetteAddress);
+
+        List<RecordedMessage> recordedMessages = emOSC.loadFromFile();
+
+        OscPlayer oscPlayer = emOSC.startPlayback(recordedMessages);
+
+        EmOSC.stopPlaybackOnUserInput(oscPlayer);
     }
 
     private static void recordAndPlayback(String[] args) throws IOException, InterruptedException {
@@ -60,64 +101,24 @@ public class EmOSC {
         int portOut = Integer.parseInt(args[2]);
         int recordingTimeSeconds = Integer.parseInt(args[3]);
         String marionetteAddress = args.length > 4 ? args[4] : "localhost";
-        recordingCountdown();
-        playback(record(portIn, recordingTimeSeconds), portOut, marionetteAddress);
+
+        EmOSC emOSC = new EmOSC(portIn, portOut, recordingTimeSeconds, marionetteAddress);
+
+        EmOSC.recordingCountdown();
+
+        List<RecordedMessage> recordedMessages = emOSC.record();
+
+        OscPlayer oscPlayer = emOSC.startPlayback(recordedMessages);
+
+        EmOSC.stopPlaybackOnUserInput(oscPlayer);
     }
 
-    @SuppressWarnings("unchecked")
-    private static List<RecordedMessage> loadFromFile(String file) throws IOException {
-        GZIPInputStream gzipInputStream = new GZIPInputStream(Files.newInputStream(Paths.get(file)));
-        ObjectInputStream objectInputStream = new ObjectInputStream(gzipInputStream);
-        try {
-            List<RecordedMessage> recordedMessages = (List<RecordedMessage>)objectInputStream.readObject();
-            objectInputStream.close();
-            gzipInputStream.close();
-            return recordedMessages;
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static List<RecordedMessage> record(int portIn, int recordingTimeSeconds) throws IOException {
-        VmcRecorder vmcRecorder = new VmcRecorder(portIn);
-        vmcRecorder.init();
-
-        System.out.println("Recording");
-        vmcRecorder.startRecording();
-        // wait for messages for the input number of seconds
-        try {
-            Thread.sleep(recordingTimeSeconds * 1000);
-        } catch (InterruptedException e) {
-            // Should never happen
-            throw new RuntimeException(e);
-        }
-        List<RecordedMessage> recordedMessages = vmcRecorder.stopRecording();
-        System.out.println("Recording stopped");
-        System.out.println("Recorded " + vmcRecorder.getMessageCount() + " messages in " + vmcRecorder.getRecordingDurationMillis() + " milliseconds");
-        return recordedMessages;
-    }
-
-    private static void saveToFile(List<RecordedMessage> recordedMessages, String file) throws IOException {
-        GZIPOutputStream gzipOutputStream = new GZIPOutputStream(Files.newOutputStream(Paths.get(file)));
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(gzipOutputStream);
-        objectOutputStream.writeObject(recordedMessages);
-        objectOutputStream.flush();
-        objectOutputStream.close();
-        gzipOutputStream.flush();
-        gzipOutputStream.close();
-    }
-
-    private static void playback(List<RecordedMessage> recordedMessages, int portOut, String marionetteAddress) throws IOException {
-        OscPlayer oscPlayer = new OscPlayer(portOut, recordedMessages);
-
-        oscPlayer.start();
-        System.out.println("Started playback");
-
-        // Keep running until
+    private static void stopPlaybackOnUserInput(OscPlayer player) {
+        // Keep running until user presses enter
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter to quit");
         if (scanner.hasNextLine()) {
-            oscPlayer.stop();
+            player.stop();
         }
     }
 
@@ -129,6 +130,58 @@ public class EmOSC {
         Thread.sleep(1000);
         System.out.println("1");
         Thread.sleep(1000);
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<RecordedMessage> loadFromFile() throws IOException {
+        GZIPInputStream gzipInputStream = new GZIPInputStream(Files.newInputStream(Paths.get(this.fileName)));
+        ObjectInputStream objectInputStream = new ObjectInputStream(gzipInputStream);
+        try {
+            List<RecordedMessage> recordedMessages = (List<RecordedMessage>) objectInputStream.readObject();
+            objectInputStream.close();
+            gzipInputStream.close();
+            return recordedMessages;
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<RecordedMessage> record() throws IOException {
+        VmcRecorder vmcRecorder = new VmcRecorder(this.portIn);
+        vmcRecorder.init();
+
+        System.out.println("Recording");
+        vmcRecorder.startRecording();
+        // wait for messages for the input number of seconds
+        try {
+            Thread.sleep(this.recordingDurationSeconds * 1000);
+        } catch (InterruptedException e) {
+            // Should never happen
+            throw new RuntimeException(e);
+        }
+        List<RecordedMessage> recordedMessages = vmcRecorder.stopRecording();
+        System.out.println("Recording stopped");
+        System.out.println("Recorded " + vmcRecorder.getMessageCount() + " messages in " + vmcRecorder.getRecordingDurationMillis() + " milliseconds");
+        return recordedMessages;
+    }
+
+    private void saveToFile(List<RecordedMessage> recordedMessages) throws IOException {
+        GZIPOutputStream gzipOutputStream = new GZIPOutputStream(Files.newOutputStream(Paths.get(this.fileName)));
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(gzipOutputStream);
+        objectOutputStream.writeObject(recordedMessages);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+        gzipOutputStream.flush();
+        gzipOutputStream.close();
+    }
+
+    private OscPlayer startPlayback(List<RecordedMessage> recordedMessages) throws IOException {
+
+        OscPlayer oscPlayer = new OscPlayer(this.portOut, recordedMessages);
+
+        oscPlayer.start();
+        System.out.println("Started playback");
+        return oscPlayer;
     }
 
 }
